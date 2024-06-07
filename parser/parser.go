c8b2d6be9d993a6c9d55fd1b1d18bab5890327f3
Tyler Lawton <17412152+super-heavy-milk@users.noreply.github.com>
2024-06-07 14:50:57 -0400

    Comment

--- parser/parser.go
+++ parser/parser.go
@@ -15,7 +15,7 @@ type Parser struct {
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{lex: l}
 
-	// This will initialize both "curToken" and "peekToken"
+	// This will initialize both `p.curToken` and `p.peekToken`
 	p.NextToken()
 	p.NextToken()
 
